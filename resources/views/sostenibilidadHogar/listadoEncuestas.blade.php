4241713e8732175b6acacfdaf64b6a69312c87af
Jorge Pineda <aplicacionesj@gmail.com>
2018-09-07 04:50:43 +0000

    Estilización de las vistas faltantes de los modulos administrativos de situr CESAR

--- resources/views/sostenibilidadHogar/listadoEncuestas.blade.php
+++ resources/views/sostenibilidadHogar/listadoEncuestas.blade.php
@@ -3,45 +3,7 @@
 
 @section('title', 'Listado de encuestas')
 
-@section('estilos')
-    <style>
-        .row {
-            margin: 1em 0 0;
-        }
-        .blank-page {
-            padding: 1em;
-        }
-        .carga {
-            display: none;
-            position: fixed;
-            z-index: 1000;
-            top: 0;
-            left: 0;
-            height: 100%;
-            width: 100%;
-            background: rgba(0, 0, 0, 0.57) url(../../Content/Cargando.gif) 50% 50% no-repeat;
-        }
-        .carga>.text{
-            position: absolute;
-            display:block;
-            text-align:center;
-            width: 100%;
-            top: 40%;
-            color: white;
-            font-size: 1.5em;
-            font-weight: bold;
-        }
-        /* Cuando el body tiene la clase 'loading' ocultamos la barra de navegacion */
-        body.charging {
-            overflow: hidden;
-        }
 
-        /* Siempre que el body tenga la clase 'loading' mostramos el modal del loading */
-        body.charging .carga {
-            display: block;
-        }
-    </style>
-@endsection
 
 @section('TitleSection', 'Listado encuestas')
 
@@ -49,29 +11,33 @@
 
 @section('controller','ng-controller="listadoEncuestasSostenibilidadHogarCtrl"')
 
-@section('content')
-    
+@section('titulo','Encuestas de sostenibilidad de hogares')
+@section('subtitulo','El siguiente listado cuenta con @{{encuestas.length}} registro(s)')
 
-<div class="container">
-    <h1 class="title1">Listar encuestas</h1>
-    <br />
-    <div class="blank-page widget-shadow scroll" id="style-2 div1">
+@section('content')
+<div class="flex-list">
+    <a href="/sostenibilidadhogares/crear" type="button" class="btn btn-lg btn-success">
+      Crear encuesta
+    </a> 
+    <div class="form-group has-feedback" style="display: inline-block;">
+        <label class="sr-only">Búsqueda de encuesta</label>
+        <input type="text" ng-model="prop.search" class="form-control input-lg" id="inputEmail3" placeholder="Buscar encuesta...">
+        <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+    </div>      
+</div>
+<div class="text-center" ng-if="(encuestas|filter:prop.search).length > 0 && (prop.search != '' && prop.search != undefined)">
+    <p>Hay @{{(encuestas|filter:prop.search).length}} registro(s) que coinciden con su búsqueda</p>
+</div>
+<div class="alert alert-info" ng-if="encuestas.length == 0">
+    <p>No hay registros almacenados</p>
+</div>
+<div class="alert alert-warning" ng-if="(encuestas|filter:prop.search).length == 0 && encuestas.length > 0">
+    <p>No existen registros que coincidan con su búsqueda</p>
+</div>
+
+        
         <div class="row">
-            <div class="col-xs-12 col-sm-4 col-lg-2 col-md-3">
-                <a href="/sostenibilidadhogares/crear" class="btn btn-primary">Crear encuesta</a>
-            </div>
-            
-            
-            <div class="col-xs-12 col-sm-12 col-lg-3 col-md-3">
-                <input type="text" style="margin-bottom: .5em;" ng-model="prop.search" class="form-control" id="inputSearch" placeholder="Buscar encuesta...">
-            </div>
-            <div class="col-xs-12 col-sm-12 col-lg-2 col-md-12" style="text-align: center;">
-                <span class="chip" style="margin-bottom: .5em;">@{{(encuestas|filter:prop.search).length}} resultados</span>
-            </div>
-        </div>
-        <br/>
-        <div class="row">
-            <div class="col-xs-12">
+            <div class="col-xs-12 table-overflow">
                 <table class="table table-striped">
                     <tr>                     
                         <th style="width: 60px;">No. de encuesta</th>
@@ -91,24 +57,24 @@
                         <td>@{{item.barrio}}</td>
                         <td>@{{item.encuestador}}</td>
                         <td>@{{item.estado}}</td>
-                        <td style="text-align: right;">@{{item.ultimaSeccion}}</td>
-                        <td style="text-align: center;"><a href="/sostenibilidadhogares/editar/@{{item.id}}" title="Editar encuesta" ng-if="item.EstadoId != 7 && item.EstadoId != 8"><span class="glyphicon glyphicon-pencil"></span></a></td>
+                        <td style="text-align: center;">@{{item.ultimaSeccion}}</td>
+                        <td style="text-align: center;">
+                            <a href="/sostenibilidadhogares/editar/@{{item.id}}" title="Editar encuesta" ng-if="item.EstadoId != 7 && item.EstadoId != 8" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span><span class="sr-only">Editar encuesta</span></a>
+                        </td>
                     </tr>
                 </table>
-                <div class="alert alert-warning" role="alert" ng-show="encuestas.length == 0 || (encuestas|filter:prop.search).length == 0">No hay resultados disponibles <span ng-show="(encuestas|filter:prop.search).length == 0">para la búsqueda '@{{prop.search}}'. <a href="#" ng-click="prop.search = ''">Presione aquí</a> para ver todos los resultados.</span></div>
+                
             </div>
             
         </div>
         <div class="row">
-          <div class="col-6" style="text-align:center;">
+          <div class="col-xs-12 text-center">
           <dir-pagination-controls pagination-id="paginacion_encuestas"  max-size="5" direction-links="true" boundary-links="true"></dir-pagination-controls>
           </div>
         </div>
-    </div>
     <div class='carga'>
 
     </div>
-</div>
 
 @endsection
 
