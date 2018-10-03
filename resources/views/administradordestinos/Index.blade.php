214da9df618c592a6bfad8b33488afafbae58dad
Jader <jadergranados@gmail.com>
2018-10-03 20:25:29 +0000

    Arreglos nuevo Layout

--- resources/views/administradordestinos/Index.blade.php
+++ resources/views/administradordestinos/Index.blade.php
@@ -6,111 +6,83 @@
 @section('estilos')
     <style>
         
-        .image-preview-input {
-            position: relative;
-            overflow: hidden;
-            margin: 0px;
-            color: #333;
-            background-color: #fff;
-            border-color: #ccc;
-        }
-
-        .image-preview-input input[type=file] {
-            position: absolute;
-            top: 0;
-            right: 0;
-            margin: 0;
-            padding: 0;
-            font-size: 20px;
-            cursor: pointer;
-            opacity: 0;
-            filter: alpha(opacity=0);
-        }
-
-        .image-preview-input-title {
-            margin-left: 2px;
-        }
-
         .messages {
             color: #FA787E;
         }
 
+        .row {
+            margin: 1em 0 0;
+        }
+        
         .input-group {
             display: flex;
         }
         .input-group-addon {
             width: 3em;
         }
-        
     </style>
 @endsection
 
 @section('TitleSection', 'Listado de destinos')
 
-@section('Progreso', '0%')
+@section('titulo','Destinos')
 
-@section('NumSeccion', '0%')
+@section('subtitulo','El siguiente listado cuenta con @{{destinos.length}} registro(s)')
 
 @section('app', 'ng-app="destinosApp"')
 
 @section('controller','ng-controller="destinosIndexController"')
-@section('titulo','Destinos')
-@section('subtitulo','El siguiente listado cuenta con @{{destinos.length}} registro(s)')
-@section('content')
-<div class="col-sm-12">
-    <div class="blank-page widget-shadow scroll" id="style-2 div1">
-        <div class="flex-list">
-            <a href="/administradordestinos/crear" type="button" class="btn btn-lg btn-success" >
-              Agregar destino
-            </a> 
-            <div class="form-group has-feedback" style="display: inline-block;">
-                <label class="sr-only">Búsqueda de destinos</label>
-                <input type="text" ng-model="prop.search" class="form-control input-lg" id="inputEmail3" placeholder="Buscar destino...">
-                <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
-            </div>      
-        </div>
-        <div class="text-center" ng-if="(destinos | filter:prop.search).length > 0 && (prop.search != '' && prop.search != undefined)">
-            <p>Hay @{{(destinos | filter:prop.search).length}} registro(s) que coinciden con su búsqueda</p>
-        </div>
-        <div class="alert alert-info" ng-if="destinos.length == 0">
-            <p>No hay registros almacenados</p>
-        </div>
-        <div class="alert alert-warning" ng-if="(destinos | filter:prop.search).length == 0 && destinos.length > 0">
-            <p>No existen registros que coincidan con su búsqueda</p>
-        </div>
-        <div class="tiles">
-            <div class="tile inline-tile" dir-paginate="destino in destinos | filter:prop.search | itemsPerPage:10" pagination-id="pagination_destinos">
-                <div class="tile-img">
-                    <img ng-src="@{{destino.multimedia_destinos.length > 0 ?  destino.multimedia_destinos[0].ruta : 'img/app/noimage.jpg'}}" alt="@{{destino.destino_con_idiomas[0].nombre}}"/>
-                </div>
-                <div class="tile-body">
-                    <div class="tile-caption">
-                        <h3>@{{destino.destino_con_idiomas[0].nombre}}</h3>
-                    </div>
-                    <p>@{{destino.destino_con_idiomas[0].descripcion}}</p>
-                    <div class="inline-buttons">
-                        <a href="/administradordestinos/editar/@{{destino.id}}" class="btn btn-warning" title="Editar">Editar</a>
-                        <button class="btn btn-@{{destino.estado ? 'danger' : 'success'}}" ng-click="desactivarActivar(destino)">@{{destino.estado ? 'Desactivar' : 'Activar'}}</button>
-                        
-                        <a href="/administradordestinos/idioma/@{{destino.id}}/@{{traduccion.idioma.id}}" ng-repeat="traduccion in destino.destino_con_idiomas" class="btn btn-default" title="@{{traduccion.idioma.culture}}"> @{{traduccion.idioma.culture}}</a>
-                        <a href="javascript:void(0)" ng-click="modalIdioma(destino)" ng-if="destino.destino_con_idiomas.length < idiomas.length" class="btn btn-default" title="Agregar idioma"> <span class="glyphicon glyphicon-plus"></span><span class="sr-only">Agregar idioma</span></a>
-                        
-                    </div>  
-                    
-                </div>
-            </div>
-        </div>
-        
-        <div class="row">
-          <div class="col-xs-12 text-center">
-          <dir-pagination-controls pagination-id="pagination_destinos"  max-size="5" direction-links="true" boundary-links="true"></dir-pagination-controls>
-          </div>
-        </div>
-    </div>
-    
-    <div class='carga'>
 
+@section('content')
+<div class="flex-list">
+    <a href="/administradordestinos/crear" role="button" class="btn btn-lg btn-success">
+      Agregar destinos
+    </a> 
+    <div class="form-group has-feedback" style="display: inline-block;">
+        <label class="sr-only">Búsqueda de destinos</label>
+        <input type="text" ng-model="prop.search" class="form-control input-lg" id="inputEmail3" placeholder="Buscar destino...">
+        <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+    </div>      
+</div>
+<div class="text-center" ng-if="(destinos | filter:prop.search).length > 0 && (prop.search != '' && prop.search != undefined)">
+    <p>Hay @{{(destinos | filter:prop.search).length}} registro(s) que coinciden con su búsqueda</p>
+</div>
+<div class="alert alert-info" ng-if="destinos.length == 0">
+    <p>No hay registros almacenados</p>
+</div>
+<div class="alert alert-warning" ng-if="(destinos | filter:prop.search).length == 0 && destinos.length > 0">
+    <p>No existen registros que coincidan con su búsqueda</p>
+</div>
+
+<div class="tiles">
+    <div class="tile inline-tile" dir-paginate="destino in destinos | filter:prop.search | itemsPerPage:10" pagination-id="pagination_destinos">
+        <div class="tile-img">
+            <img ng-src="@{{destino.multimedia_destinos.length > 0 ?  destino.multimedia_destinos[0].ruta : 'img/app/noimage.jpg'}}" alt="@{{destino.destino_con_idiomas[0].nombre}}"/>
+        </div>
+        <div class="tile-body">
+            <div class="tile-caption">
+                <h3>@{{destino.destino_con_idiomas[0].nombre}}</h3>
+            </div>
+            <p>@{{destino.destino_con_idiomas[0].descripcion | limitTo:255}}<span ng-if="destino.destino_con_idiomas[0].descripcion.length > 255">...</span></p>
+            <div class="inline-buttons">
+                <a href="/administradordestinos/editar/@{{destino.id}}" class="btn btn-warning">Editar</a>
+                <button class="btn btn-@{{destino.estado ? 'danger' : 'success'}}" ng-click="desactivarActivar(destino)">@{{destino.estado ? 'Desactivar' : 'Activar'}}</button>
+                <a href="/administradordestinos/idioma/@{{destino.id}}/@{{traduccion.idioma.id}}" class="btn btn-default" ng-repeat="traduccion in destino.destino_con_idiomas"> @{{traduccion.idioma.culture}}</a>
+                <button type="button" ng-click="modalIdioma(destino)" class="btn btn-default" ng-if="destino.destino_con_idiomas.length < idiomas.length"> <span class="glyphicon glyphicon-plus"></span><span class="sr-only">Agregar idioma</span></button>
+            </div>  
+            
+        </div>
     </div>
+</div>
+
+<div class="row">
+  <div class="col-xs-12 text-center">
+      <dir-pagination-controls pagination-id="pagination_destinos"  max-size="5" direction-links="true" boundary-links="true"></dir-pagination-controls>
+  </div>
+</div>
+    
+<div class='carga'>
+
 </div>
 
 <div class="modal fade" tabindex="-1" role="dialog" id="idiomaModal">
@@ -118,8 +90,8 @@
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Nuevo idioma para el destino</h4>
-                </div>
+                <h4 class="modal-title">Nuevo idioma para la atracción</h4>
+            </div>
             <form>
                 <div class="modal-body">
                     <div class="form-group">
@@ -130,14 +102,13 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
-                    <button type="button" ng-click="nuevoIdioma()" class="btn btn-success">Guardar</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                    <button type="button" ng-click="nuevoIdioma()" class="btn btn-success">Enviar</button>
                 </div>
             </form>
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
-
 @endsection
 
 @section('javascript')
