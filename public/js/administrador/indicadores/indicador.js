373fca9f08166951e6ab9f39925328e51508033b
Jhon Trespalacios <jtrespalaciosfernandez@gmail.com>
2018-12-11 14:27:20 +0000

    Calculo de indicadores

---
+++ public/js/administrador/indicadores/indicador.js
@@ -0,0 +1,55 @@
+var pp=angular.module('admin.indicadores', ['serviceIndicadores','angularUtils.directives.dirPagination'])
+
+.controller('indicadoresCtrl', ['$scope','serviceIndicador',function ($scope,serviceIndicador) {
+    
+     $("body").attr("class", "cbp-spmenu-push charging");
+    serviceIndicador.getInfo()
+        .then(function(data){
+             $("body").attr("class", "cbp-spmenu-push");
+            $scope.anios = data.anios;
+            $scope.meses = data.meses;
+            $scope.indicadores = data.indicadores;
+            $scope.tiposMedicion = data.tiposMedicion;
+            $scope.indicadoresMedicion = data.indicadoresMedicion;
+        })
+        .catch(function(){
+            $("body").attr("class", "cbp-spmenu-push");
+            swal("Error", "Error en la carga, por favor recarga la pagina", "error");
+        });
+
+    $scope.abrirModal = function(){
+        $scope.indicador={};
+        $scope.indicadorForm.$setPristine();
+        $scope.indicadorForm.$setUntouched();
+        $('#calcularIndicadores').modal();
+        
+    }
+    
+    $scope.guardar = function(){
+        
+        if(!$scope.indicadorForm.$valid){
+            swal("Error","Corrija los errores","error");
+            return;
+        }
+        
+        $("body").attr("class", "cbp-spmenu-push charging");
+        serviceIndicador.guardar($scope.indicador)
+        .then(function(data){
+             $("body").attr("class", "cbp-spmenu-push");
+            if(data.success){
+                swal("Exito","Se ha calculado exitosamente","success");
+                $scope.indicadoresMedicion = data.indicadoresMedicion;
+                $('#calcularIndicadores').modal('hide');
+            }else{
+                
+                swal("Error","Error en el cálculo","error");
+                $scope.errores = data.errores;
+            }
+        })
+        .catch(function(){
+            $("body").attr("class", "cbp-spmenu-push");
+            swal("Error", "Error en la carga, por favor recarga la pagina", "error");
+        });
+    }
+}])
+
