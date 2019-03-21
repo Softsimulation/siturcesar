10cc8fb28033c53c00b504422dd07c052c0d9b9b
Jorge Pineda <aplicacionesj@gmail.com>
2019-03-21 20:54:40 +0000

    Se corrigió la vista de detalle de destinos

--- resources/views/rutas/Ver.blade.php
+++ resources/views/rutas/Ver.blade.php
@@ -26,4 +26,65 @@
         </div>
         @endforeach
     </div>
+    <div id="map" style="height: 400px;"></div>
+@endsection
+@section('javascript')
+<script async defer
+   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC55uUNZFEafP0702kEyGLlSmGE29R9s5k&callback=initMap">
+</script>
+<script>
+    function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var lat = 9.2700371, long = -74.6345401;
+      // The location of Uluru
+      var pos = {lat: lat, lng: long};
+      // The map, centered at Uluru
+      var map = new google.maps.Map(
+          document.getElementById('map'), {zoom: 8, center: pos});
+      // The marker, positioned at Uluru
+      //var marker = new google.maps.Marker({position: pos, map: map});
+      directionsDisplay.setMap(map);
+      
+      //calculateAndDisplayRoute(directionsService, directionsDisplay);
+    }
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        var waypts = [];
+        var checkboxArray = document.getElementById('waypoints');
+        for (var i = 0; i < checkboxArray.length; i++) {
+          if (checkboxArray.options[i].selected) {
+            waypts.push({
+              location: checkboxArray[i].value,
+              stopover: true
+            });
+          }
+        }
+
+        directionsService.route({
+          origin: document.getElementById('start').value,
+          destination: document.getElementById('end').value,
+          waypoints: waypts,
+          optimizeWaypoints: true,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            var route = response.routes[0];
+            var summaryPanel = document.getElementById('directions-panel');
+            summaryPanel.innerHTML = '';
+            // For each route, display summary information.
+            for (var i = 0; i < route.legs.length; i++) {
+              var routeSegment = i + 1;
+              summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                  '</b><br>';
+              summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+              summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+              summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+            }
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+</script>
 @endsection
