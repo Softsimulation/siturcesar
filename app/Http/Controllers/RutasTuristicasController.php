f02fbe7de89cb22681132cd93534a0c116cceafa
Jorge Pineda <aplicacionesj@gmail.com>
2019-03-21 23:16:07 +0000

    Se estilizó rutas y se corrigió destinos

--- app/Http/Controllers/RutasTuristicasController.php
+++ app/Http/Controllers/RutasTuristicasController.php
@@ -18,17 +18,22 @@ class RutasTuristicasController extends Controller
         }
         
         $ruta = Ruta::where('id', $id)->with(['rutasConIdiomas' => function ($queryRutasConIdiomas){
-            $queryRutasConIdiomas->orderBy('idioma_id')->select('idioma_id', 'ruta_id', 'nombre', 'descripcion', 'recomendacion');
+            $queryRutasConIdiomas->where('idioma_id', 1)->select('idioma_id', 'ruta_id', 'nombre', 'descripcion', 'recomendacion');
         }, 'rutasConAtracciones' => function ($queryRutasConAtracciones){
             $queryRutasConAtracciones->with(['sitio' => function($querySitio){
                 $querySitio->with(['sitiosConIdiomas' => function($querySitiosConIdiomas){
-                    $querySitiosConIdiomas->select('idiomas_id', 'sitios_id', 'nombre');
+                    $querySitiosConIdiomas->where('idiomas_id', 1)->select('idiomas_id', 'sitios_id', 'nombre');
                 }, 'multimediaSitios' => function($queryMultimediaSitios){
                     $queryMultimediaSitios->select('sitios_id', 'ruta')->orderBy('portada', 'desc')->where('tipo', false);
                 }])->select('id', 'latitud', 'longitud');
             }])->select('atracciones.id', 'atracciones.sitios_id');
         }])->select('id', 'portada')->first();
         
+        $ruta->rutasConIdiomas = $ruta->rutasConIdiomas[0];
+        foreach($ruta->rutasConAtracciones as $atraccion){
+            $atraccion->sitio->sitiosConIdiomas = $atraccion->sitio->sitiosConIdiomas[0];
+        }
+        //return $ruta->rutasConIdiomas->nombre;
         //return ['ruta' => $ruta];
         return view('rutas.Ver', ['ruta' => $ruta]);
     }
