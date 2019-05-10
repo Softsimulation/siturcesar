cd71747d91ceaac6969230851f8b0c2bad69d83f
Andres Paternina <eduardoandrespaternina@gmail.com>
2019-05-10 19:54:13 +0000

    Cambios de vista de cesar con arreglo de actividades a centro en valledupar

--- resources/views/actividades/Ver.blade.php
+++ resources/views/actividades/Ver.blade.php
@@ -414,7 +414,7 @@ function parse_yturl($url)
     function initMap() {
           var sitiosConActividades = <?php print($actividad->sitiosConActividades); ?>;
           console.log(sitiosConActividades);
-          var lat = 10.1287919, long = -75.366555;
+          var lat = 10.3198872, long = -73.521013;
           var posInit = {lat: lat, lng: long};
           // Initialize and add the map
           var map = new google.maps.Map(
---
+++ resources/views/home/soporte.blade.php
@@ -0,0 +1,12 @@
+@extends('layout._publicLayout')
+		<br><br><br><br><br><br><br><br>
+		<div  class='container'>
+			<div class='row'>
+				
+				<h1> Página de soporte</h1><br>
+				<div class="col-*-*" >
+				En el caso de tener un problema, debe contactar con el equipo de Situr Cesar al correo direccion@siturcesar.com
+				</div>
+					
+			</div>
+		</div>
